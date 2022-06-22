3b887d557cb4553a2aa7a7bf7494fe0aa1d2ffcc
BerkeAnna <berke.anna0803@gmail.com>
2022-06-22 12:36:53 +0200

    chapter 1

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,93 @@
+/*let age = 255;
+let year = 2001;
+
+console.log(age, year);
+
+age=30;
+console.log(age);
+
+const point = 100; //we can't modify const variables, it gets mistake. example in the next line: point = 50;
+console.log(point);
+
+var score= 50;
+console.log(score);
+
+console.log("Hello world");
+
+let email = 'email@email.hu';
+console.log(email);
+
+let firstName = "Anna";
+let surName = "Berke";
+let fullName = firstName + " " + surName;
+console.log(fullName);
+console.log(firstName[0]);
+console.log(fullName.length);
+console.log(fullName.toUpperCase()); 
+let res =  fullName.toLowerCase();
+console.log(res);
+console.log(res, fullName);
+
+let indexOfEmail = email.indexOf('em');
+console.log(indexOfEmail);
+console.log(email.lastIndexOf('l'));
+let sliceEmail= email.slice(5,10);
+console.log(sliceEmail);
+
+let sliceEmail2 = email.substr(0,10);
+console.log(sliceEmail2);
+
+let replaceEmail = email.replace('e', 'q');
+console.log(replaceEmail);
+
+
+let radius = 10;
+const pi = 3.14;
+console.log(radius, pi); 
+console.log(10/5); 
+console.log(radius % 6); 
+console.log(pi * radius ** 2); 
+console.log((pi * radius) ** 2); 
+
+let radisuplusone = radius+1;
+console.log(radisuplusone); 
+radius++;
+console.log(radius); 
+radius--;
+console.log(radius); 
+radius*=2;
+console.log(radius); 
+radius/=2;
+console.log(radius); 
+
+let s= "The blog has ";
+let s2 = " likes";
+
+console.log(s + radius + s2);
+
+
+let title = "Webshop";
+let author = "Anna";
+let costumers = 20;
+
+let res = `The web name is ${title} and the author is ${author} and it has ${costumers} costumers`;
+console.log(res);
+console.log(ninjas.length);
+
+
+let ninjas = ['shuan','ryu', 'chun-li'];
+
+console.log(ninjas.join(','));
+console.log(ninjas.indexOf('ryu'));
+console.log(ninjas.concat('ryu', 'ron'));
+console.log(ninjas.push('ken'));  
+console.log(ninjas.pop());  
+console.log(ninjas.pop());  
+console.log(ninjas);
+*/
+
+console.log(true, false); 
+let email = 'a@com.hu';
+let res = email.includes('@');
+let res2 = email.includes(']');
+console.log(res, res2);
